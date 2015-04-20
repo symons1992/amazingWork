30dcb7e9dd3f20985dc1516ad0ea7ef5b17ef60d
symson1992 <symons@symons-laptop.(none)>
2015-04-21 00:32:24 +0800

    update code

---
+++ models.py
@@ -0,0 +1,23 @@
+#-*- coding:utf -*-
+
+class People:
+    def __init__(self):
+        self.id   = ''
+        self.homepage = ''
+        self.name = ''
+        self.age  = '' 
+        self.contact = []  
+        self.contact_ids = []
+        self.rev_contact = []
+        self.rev_contact_ids = []
+        self.collection_article_title = []
+        self.collection_article_url   = []
+        self.group_joins = []
+
+    def count_contact(self):
+        return len(self.contact)
+
+    def count_rev_contact(self):
+        return len(self.contact)
+
+
--- parser.py
+++ parser.py
@@ -6,5 +6,13 @@ import re
 
 def RelationParser(content):
     pass
+
+def contactParser(content):
+    # 主要作用就是解析用户关注/被关注人的信息
+    cp = re.compile('<dd><a href="([^"]+)">([^<]+)</a></dd>')
+    contactList = cp.findall(content)
+    peopleList = [ item[1] for item in contactList ]
+    return peopleList
     
+
     
